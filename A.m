358849c33da53beb4227a067868259b860f5181d
HU-qingqing <75147153+HU-qingqing@users.noreply.github.com>
2021-04-19 01:43:05 +0200

    Create A.m

---
+++ A.m
@@ -0,0 +1,24 @@
+function val = mat_A(x,y)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% mat_A :
+% Evaluation de la matrice A .
+%
+% SYNOPSIS val = mat_A(x,y)
+%          
+% INPUT * x,y : les 2 coordonnees du point ou on veut evaluer la fonction.
+%
+% OUTPUT - val: valeur de la matrice sur ce point.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%A COMPLETER
+val = [1 0;0 1];
+%val = (2+sin(8*pi*x)*sin(8*pi*y))*[1 0;0 1];
+%val = [2+sin(2*pi*x/10) 0;0 2+sin(2*pi*x/10)];
+
+%val = [2+sin(2*pi*x/100) 0;0 2+sin(2*pi*x/100)];
+%
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                     fin de la fonction
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
